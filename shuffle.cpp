4d60af9fe75abd934bfb9ce0b4f74064b1fb83d2
showtimemmc <showtimemmc@hotmail.com>
2014-04-17 16:29:26 +0800

    use template function

--- shuffle.cpp
+++ shuffle.cpp
@@ -3,6 +3,8 @@
  */
 #include <iostream>
 #include <ctime>
+#include <string>
+/* 
 void shuffle(int* poker,int length)
 {
 	srand((unsigned int)time(0));
@@ -14,15 +16,36 @@ void shuffle(int* poker,int length)
 		poker[random]=tmp;
 	}
 }
+*/
+template <typename T>
+void shuffle(T* poker,int length)
+{
+	srand((unsigned int)time(0));
+	for (int i = 0; i < length; ++i)
+	{
+		int random=rand()%length;
+		T tmp=poker[i];
+		poker[i]=poker[random];
+		poker[random]=tmp;
+	}
+}
 
 int main(int argc, char const *argv[])
 {
-	int poker[10]={1,2,3,4,5,6,7,8,9,10};
-	shuffle(poker,10);
+	int i_poker[10]={1,2,3,4,5,6,7,8,9,10};
+	char c_poker[10]={'a','b','c','d','e','f'};
+	shuffle(i_poker,10);
+	shuffle(c_poker,6);
 	for (int i = 0; i < 10; ++i)
 	{
-		std::cout<<poker[i]<<' ';
+		std::cout<<i_poker[i]<<' ';
 	}
 	std::cout<<std::endl;
+	for (int i = 0; i < 6; ++i)
+	{
+		std::cout<<c_poker[i]<<' ';
+	}
+	std::cout<<std::endl;
+	
 	return 0;
 }
