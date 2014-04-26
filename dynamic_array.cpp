79fbfeb14cc86ff0704a18a19ea33b4f7e31c08f
showtimemmc <showtimemmc@hotmail.com>
2014-04-26 15:54:56 +0800

    more func

--- dynamic_array.cpp
+++ dynamic_array.cpp
@@ -7,48 +7,115 @@ struct MutableArray
 };
 /**
  * Initinalize the mutable array
- * @param  ma   [description]
- * @param  size [description]
+ * @param  ma             
+ * @param  init_size  
  * @return      True|False    return true if it is initialized success
  */
-bool InitMutableArray(MutableArray* ma,size_t init_size)
+bool InitMutableArray(MutableArray* pma,size_t init_size)
 {
-	ma->array=(int*)malloc(init_size*sizeof(MutableArray));
-	if (ma->array == NULL)
+	pma->array=(int*)malloc(init_size*sizeof(int));
+	if (pma->array == NULL)
 	{
-		std::cout<<"mem assign failed!"<<std::endl;
+		std::cout<<"ERROR,mem assign failed!"<<std::endl;
 		return false;
 	}
-	ma->used=0;
-	ma->size=init_size;
+	pma->used=0;
+	pma->size=init_size;
+
+	//std::cout<<"after init: "<<ma->array<<std::endl;  //new value
+	//std::cout<<"after init&: "<<&ma->array<<std::endl;//same with struct address
+
 	return true; 
 }
-void FreeMutableArray(MutableArray* ma)
+/**
+ * [FreeMutableArray description]
+ * @param ma [description]
+ */
+void FreeMutableArray(MutableArray* pma)
 {
-	free(ma->array);
-	ma->used=0;
-	ma->size=0;
+	free(pma->array);
+	pma->array=NULL;
+	pma->used=0;
+	pma->size=0;
 }
+/**
+ * 在数组末尾添加对象
+ * @param  pma     [description]
+ * @param  int_obj [description]
+ * @return         True|False    return true if successed
+ */
+bool AddObject(MutableArray* pma,int int_obj)
+{
+	//XXX:size会随着数组规模的扩大，成指数增长
+	if (pma->used == pma->size)		
+	{
+		pma->size=pma->size*2;
+	}
+	//ma->array=realloc(ma->array,ma->size);  the address of ma->array would be lost,if the realloc failed.
+	if (!realloc(pma->array,pma->size))
+	{
+		printf("ERROR,mem assign failed\n");
+		return false;
+	}
+	pma->array[pma->used++]=int_obj;
+	return true;
+}
+/**
+ * get object at index in array
+ * @param  pma    [description]
+ * @param  index [description]
+ * @return       [description]
+ */
+int GetObject(MutableArray* pma,size_t index)
+{
+	if (index >= pma->used)//负数参数传入时，由于传递参数副本时进行了类型转换，负数被转换为很大的正数，所以不需要判断index小于0的状况
+	{
+		//printf("index:%u\n",index);
+		printf("ERROR,check the param index\n");
+		exit(1);
+	}
+	else
+	{
+		return pma->array[index];
+	}
+}
+//TODO: remove function
+//TODO: support multi type with template
+//TODO: class
 int main(int argc, char const *argv[])
 {
 	MutableArray ma;
-	InitMutableArray(&ma,5);
-	/*
-	if (!InitMutableArray(ma,5))		
+
+	//std::cout<<"&ma: "<<&ma<<std::endl;  //address of struct var ma
+	//std::cout<<"before init ma.array: "<<ma.array<<std::endl; //random value
+	//std::cout<<"&ma.array: "<<&ma.array<<std::endl;  //same with struct var ,ma 
+
+	if (!InitMutableArray(&ma,5))		
 	{
 		printf("Initinalize Failed!\n");
 		return 1;
-	}*/
-	for (int i = 0; i < ma->used; ++i)
-	{
-		ma->array[i]=3;
 	}
-	for (int i = 0; i < ma->used; ++i)
+	for (size_t i = 0; i < ma.size; ++i)
 	{
-		printf("%i\n", ma->array[i]);
+		ma.array[i]=3;
+		ma.used++;
 	}
+	for (size_t i = 0; i < ma.size; ++i)
+	{
+		printf("%d\n", ma.array[i]);
+	}
+	std::cout<<"used:"<<ma.used<<std::endl;
+	AddObject(&ma,8);
+	std::cout<<"used:"<<ma.used<<std::endl;
+
+	int index;
+	std::cin>>index;
+	std::cout<<"get:"<<GetObject(&ma,index)<<std::endl;
+
+	std::cout<<ma.array<<std::endl;
+
 	FreeMutableArray(&ma);
-	std::cout<<ma->array<<std::endl;
+	std::cout<<ma.array<<std::endl;
 	return 0;
 }
 
