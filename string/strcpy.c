d30aa52b199518eb72734d8a3b9ce552b610e88a
showtimemmc <showtimemmc@hotmail.com>
2014-05-06 00:03:00 +0800

    first with bug

--- string/strcpy.c
+++ string/strcpy.c
@@ -1,26 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * [mystrcpy description]
+ * @param  src  [description]
+ * @param  dest [description]
+ * @return      [description]
+ * BUGFIX:两个字符串地址重合时，拷贝出错
+ */
 char* mystrcpy(const char* src,char* dest)
 {
 	if (!src)
 	{
+		printf("src is null");
 		return NULL;
 	}
-	char* cursor=src;
 	int i=0;
-	while(cursor[i] != '\0')
+	while(src[i] != '\0')
 	{
-		dest[i]=*cursor;
-		i++;
-		cursor++;		
+		dest[i]=src[i];
+		i++;		
 	}
 	return dest;
 }
 int main(int argc, char const *argv[])
 {
 	char* src="hello";
-	char* dest="It is a  new world";
-
+	char dest[]="aha bingo";
+	printf("%s\n", dest);
 	mystrcpy(src,dest);
 	printf("%s\n", dest);
 	return 0;
