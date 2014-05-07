28d63b64cbba78eba9f05ae012b1d2295ecffd98
showtimemmc <showtimemmc@hotmail.com>
2014-05-07 23:53:42 +0800

    bug fixed

--- string/strcpy.c
+++ string/strcpy.c
@@ -9,9 +9,9 @@
  */
 char* mystrcpy(const char* src,char* dest)
 {
-	if (!src)
+	if (src ==NULL||dest==NULL)//assert(src!=NULL)&&assert(dest!=NULL)
 	{
-		printf("src is null");
+		printf("param error");
 		return NULL;
 	}
 	int i=0;
@@ -20,12 +20,27 @@ char* mystrcpy(const char* src,char* dest)
 		dest[i]=src[i];
 		i++;		
 	}
+	dest[i]='\0';//防止输出dest地址上，新复制的字符串之后的字符
 	return dest;
 }
+
+char* mystrcpy2(const char* src,char* dest)
+{
+	if (src== NULL||dest==NULL)
+	{
+		printf("param error");
+		return NULL;
+	}
+	char* ret=dest;
+	while((*dest++=*src++)!='\0');
+	return ret;
+
+}
+
 int main(int argc, char const *argv[])
 {
 	char* src="hello";
-	char dest[]="aha bingo";
+	char dest[40];
 	printf("%s\n", dest);
 	mystrcpy(src,dest);
 	printf("%s\n", dest);
