ff7ba4b8cd9ca6be55cfe793eec3d6eba9a3d73c
showtimemmc <showtimemmc@hotmail.com>
2014-06-05 23:37:05 +0800

    异或妙用

--- xor.cpp
+++ xor.cpp
@@ -5,6 +5,7 @@
 int main(int argc, char const *argv[])
 {
 	int a[]={3,2,2,1,3,1,5,6,5,5,5};
+	
 	for (int i = 1; i < 11; ++i)
 	{
 		a[0]^=a[i];
@@ -13,3 +14,7 @@ int main(int argc, char const *argv[])
 	std::cout<<"Final :"<<a[0]<<std::endl;
 	return 0;
 }
+/*简单证明
+*x^x=0,x^0=x,异或满足交换律
+*所以通过交换律，只要是成对出现的数都会异或为0，结果只剩下1个不同的数
+*/
